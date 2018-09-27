1d8ad99e1adb9b5a11eaaa64457331153710b27d
Ruslan S <ruslanshuranov@gmail.com>
2018-09-27 21:17:47 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ gulp.task('styles', function () {
     .pipe(plumber({errorHandler: handleError}))
     .pipe(concat('main.min.css'))
     .pipe(sourcemaps.init())
-    // .pipe(scss({outputStyle: 'compressed', includePaths: SASS_INCLUDE_PATHS}))
+    .pipe(scss({outputStyle: 'compressed', includePaths: SASS_INCLUDE_PATHS}))
     .pipe(scss({includePaths: SASS_INCLUDE_PATHS}))
     .pipe(autoprefixer({browsers: ['last 2 versions', 'safari 8', 'ie 11', 'opera 12.1', 'ios 6', 'android 4']}))
     .pipe(sourcemaps.write())
